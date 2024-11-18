af5427847c6d6e8aa8df3c1293843d20c7e5b2f6
brewdium <spaxcell@gmail.com>
2024-11-18 15:15:40 +1030

    add split hand pin config

--- keyboards/handwired/dactyl_manuform/4x6_5/config.h
+++ keyboards/handwired/dactyl_manuform/4x6_5/config.h
@@ -25,6 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MASTER_LEFT
 #define SPLIT_USB_DETECT
+#define SPLIT_HAND_PIN F5
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
