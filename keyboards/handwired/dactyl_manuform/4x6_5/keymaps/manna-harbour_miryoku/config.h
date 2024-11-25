ae6f04d6de24578697054ae9b81664f56f363817
brewdium <spaxcell@gmail.com>
2024-11-26 08:56:41 +1030

    add reboot key and fixed layout

--- keyboards/handwired/dactyl_manuform/4x6_5/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/handwired/dactyl_manuform/4x6_5/keymaps/manna-harbour_miryoku/config.h
@@ -3,7 +3,6 @@
 // generated -*- buffer-read-only: t -*-
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 #pragma once
 
 #define XXX KC_NO
@@ -20,5 +19,5 @@ KC_TAB,   K00,      K01,      K02,      K03,      K04,                K05,      
 KC_LSFT,  K10,      K11,      K12,      K13,      K14,                K15,      K16,      K17,      K18,      K19,      KC_ENT, \
 KC_LCTL,  K20,      K21,      K22,      K23,      K24,                K25,      K26,      K27,      K28,      K29,      KC_RCTL, \
                               K32,      K33,      K34,                K35,      K36,      K37, \
-                                        KC_LALT,  KC_LGUI,            KC_HOME,  KC_END \
+                                        KC_LALT,  QK_REBOOT,          KC_RGUI,  KC_MEDIA_PLAY_PAUSE \
 )
